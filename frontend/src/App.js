1ca08ff61867c6141726a07c8a110c00c2a673a7
zjamal04 <157428174+zjamal04@users.noreply.github.com>
2024-10-24 22:07:35 -0400

    finished react setup (#8)
    * finished react setup
    
    * fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import { fetchArtists } from './api';
 
+
 function App() {
     const [artists, setArtists] = useState([]);
 
@@ -11,16 +12,6 @@ function App() {
             .catch((error) => console.error(error));
     }, []);
 
-    return (
-        <div>
-            <h1>Spotify Wrapped Artists</h1>
-            <ul>
-                {artists.map((artist) => (
-                    <li key={artist.id}>{artist.name} - {artist.genre}</li>
-                ))}
-            </ul>
-        </div>
-    );
 }
 
 export default App;
