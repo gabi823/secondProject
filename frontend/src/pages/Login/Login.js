2361d8b7f58b29a55e85d1c1b34a14853f98af22
Gabrielle Long <gabrielle.long2013@gmail.com>
2024-10-28 22:51:44 -0400

    create account and login pages, started selection

---
+++ frontend/src/pages/Login/Login.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import NavBar from "../../components/NavBar/NavBar";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    return (
+        <>
+            <NavBar />
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    marginTop: 175,
+                }}
+            >
+                <div
+                    style={{
+                        width: 400,
+                        padding: '60px 85px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        background: 'white',
+                        borderRadius: 20,
+                        border: '1px solid black',
+                    }}
+                >
+                    <div
+                        style={{
+                            color: 'black',
+                            fontSize: 24,
+                            fontFamily: 'Manrope',
+                            fontWeight: '600',
+                            textAlign: 'center',
+                            marginBottom: '20px',
+                        }}
+                    >
+                        Login
+                    </div>
+                    <div
+                        style={{
+                            fontSize: 18,
+                            fontFamily: 'Manrope',
+                            fontWeight: '500',
+                            color: 'black',
+                            alignSelf: 'flex-start',
+                            marginBottom: '5px',
+                        }}
+                    >
+                        Username:
+                    </div>
+                    <input
+                        type="text"
+                        style={{
+                            width: '100%',
+                            height: '30px',
+                            fontSize: '18px',
+                            border: 'none',
+                            borderBottom: '1px solid #000',
+                            outline: 'none',
+                            marginBottom: '20px',
+                            fontFamily: 'Manrope',
+                        }}
+                    />
+                    <div
+                        style={{
+                            fontSize: 18,
+                            fontFamily: 'Manrope',
+                            fontWeight: '500',
+                            color: 'black',
+                            alignSelf: 'flex-start',
+                            marginBottom: '5px',
+                        }}
+                    >
+                        Password:
+                    </div>
+                    <input
+                        type="password"
+                        style={{
+                            width: '100%',
+                            height: '30px',
+                            fontSize: '18px',
+                            border: 'none',
+                            borderBottom: '1px solid #000',
+                            outline: 'none',
+                            marginBottom: '40px',
+                            fontFamily: 'Manrope',
+                        }}
+                    />
+                    <button
+                        style={{
+                            width: '60%',
+                            height: '40px',
+                            background: 'white',
+                            borderRadius: 5,
+                            border: '1px solid #000',
+                            fontSize: 18,
+                            fontFamily: 'Manrope',
+                            fontWeight: '600',
+                            color: 'black',
+                            cursor: 'pointer',
+                            transition: 'background 0.3s',
+                        }}
+                        onMouseEnter={(e) => (e.target.style.background = '#f0f0f0')}
+                        onMouseLeave={(e) => (e.target.style.background = 'white')}
+                    >
+                        Login
+                    </button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Login;
