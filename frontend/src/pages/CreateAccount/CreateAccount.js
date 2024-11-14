84a0588b01cbdc73d03b5fa6f01ad93af506b3d7
Gabrielle Long <gabrielle.long2013@gmail.com>
2024-11-13 22:57:12 -0500

    animation for login/create account pages

--- frontend/src/pages/CreateAccount/CreateAccount.js
+++ frontend/src/pages/CreateAccount/CreateAccount.js
@@ -42,29 +42,48 @@ const CreateAccount = () => {
 
     return (
         <>
-            <NavBar />
+            <NavBar/>
             <div className="images-container">
                 <div className="images-row">
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 1" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 2" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 3" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 4" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 5" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 6" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 7" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 8" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 9" />
+                    <img className="imageTop image1" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 1"/>
+                    <img className="imageTop image2" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 2"/>
+                    <img className="imageTop image3" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 3"/>
+                    <img className="imageTop image4" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 4"/>
+                    <img className="imageTop image5" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 5"/>
+                    <img className="imageTop image6" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 6"/>
+                    <img className="imageTop image7" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 7"/>
+                    <img className="imageTop image8" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 8"/>
+                    <img className="imageTop image9" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 9"/>
+
                 </div>
-                <div className="images-row second">
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 9" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 10" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 11" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 12" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 13" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 14" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 15" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 16" />
-                    <img className="image" src="https://via.placeholder.com/161x161" alt="Placeholder image 17" />
+                <div className="images-row-second">
+                    <img className="imageBottom image1" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 9"/>
+                    <img className="imageBottom image2" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 10"/>
+                    <img className="imageBottom image3" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 11"/>
+                    <img className="imageBottom image4" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 12"/>
+                    <img className="imageBottom image5" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 13"/>
+                    <img className="imageBottom image6" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 14"/>
+                    <img className="imageBottom image7" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 15"/>
+                    <img className="imageBottom image8" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 16"/>
+                    <img className="imageBottom image9" src="https://via.placeholder.com/161x161"
+                         alt="Placeholder image 17"/>
                 </div>
             </div>
             <div className="create-account-container">
