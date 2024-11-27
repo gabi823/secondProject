005f31ca379547360e9caaca98bf25d28f92ea03
Gabrielle Long <gabrielle.long2013@gmail.com>
2024-11-26 22:39:43 -0500

    better layout for settings.py

--- secondProject/secondProject/wsgi.py
+++ secondProject/secondProject/wsgi.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'secondProject.secondProject.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'secondProject.secondProject.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secondProject.settings')
 
 application = get_wsgi_application()
