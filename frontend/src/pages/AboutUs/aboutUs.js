8e51a2cab52988819fc287ad4f785002839aca0f
Gabrielle Long <gabrielle.long2013@gmail.com>
2024-11-30 15:20:54 -0500

    nav bar on the about page

--- frontend/src/pages/AboutUs/aboutUs.js
+++ frontend/src/pages/AboutUs/aboutUs.js
@@ -1,12 +1,51 @@
 import React, { useEffect } from "react";
 import NavBarLoggedIn from "../../components/NavBarLoggedIn/NavBarLoggedIn";
+import NavBar from "../../components/NavBar/NavBar";
 import emailjs from '@emailjs/browser';
 import "./aboutUs.css";
+import NavBar from "../../components/NavBar/NavBar";
 
 const AboutUs = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
+    const checkAuthStatus = async () => {
+            try {
+                // Get the token from localStorage
+                const token = localStorage.getItem('token');
+
+                // If there's no token, user is not logged in
+                if (!token) {
+                    setIsLoggedIn(false);
+                    return;
+                }
+
+                // Check login status with the token
+                const response = await axios.get("https://secondproject-8lyv.onrender.com/api/check-login/", {
+                    headers: {
+                        'Authorization': `Token ${token}`
+                    },
+                    withCredentials: true
+                });
+
+                if (response.data.isLoggedIn) {
+                    setIsLoggedIn(true);
+                    setUser(response.data.user);
+                } else {
+                    setIsLoggedIn(false);
+                    localStorage.removeItem('token');
+                }
+            } catch (error) {
+                console.error("Error checking login status:", error);
+                setIsLoggedIn(false);
+                localStorage.removeItem('token');
+            }
+        };
+
+
     // Initialize EmailJS with your User ID
-    emailjs.init(process.env.REACT_APP_EMAIL_JS_USER_ID); // Replace with your actual public key
+    emailjs.init(process.env.REACT_APP_EMAIL_JS_USER_ID);// Replace with your actual public key
+    checkAuthStatus();
   }, []);
 
   const handleSubmit = (event) => {
@@ -46,7 +85,7 @@ const AboutUs = () => {
 
   return (
       <>
-        <NavBarLoggedIn />
+        {isLoggedIn ? <NavBarLoggedIn /> : <NavBar />}
           <h1 className="message">We hope you enjoyed Nostalgify! 🎵</h1>
           <p className="link-message">
             Click <a href="https://group22project.weebly.com" target="_blank" rel="noopener noreferrer" className="link">here</a> to
