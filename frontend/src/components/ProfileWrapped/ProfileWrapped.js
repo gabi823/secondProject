8c03c9cb5a40d2e247c989dd9ffc0416bcfa7051
Gabrielle Long <gabrielle.long2013@gmail.com>
2024-12-01 18:58:56 -0500

    profile wrapped darkmode

--- frontend/src/components/ProfileWrapped/ProfileWrapped.js
+++ frontend/src/components/ProfileWrapped/ProfileWrapped.js
@@ -19,37 +19,37 @@ const ProfileWrapped = ({ albumCoverUrl, wrappedName, dateCreated, onDelete }) =
 
     return (
         <>
-        <div className="wrapped-section">
-            <img
-                src={albumCoverUrl || "https://via.placeholder.com/160x160"}
-                alt={`Wrapped #${wrappedName}`}
-                className="wrapped-image"
-            />
-            <div className="wrapped-title">
-                {wrappedName}
-                <br/>
-                <span className="wrapped-date">Date Created: {dateCreated}</span>
+            <div className="wrapped-section">
+                <img
+                    src={albumCoverUrl}
+                    alt={`Wrapped #${wrappedName}`}
+                    className="wrapped-image"
+                />
+                <div className="wrapped-title">
+                    {wrappedName}
+                    <br/>
+                    <span className="wrapped-date">Date Created: {dateCreated}</span>
+                </div>
+
+                <span className="delete" onClick={handleDeleteClick}>&times;</span>
+                <span className="divider"></span>
             </div>
 
-            <span className="delete" onClick={handleDeleteClick}>&times;</span>
-            <span className="divider"></span>
-        </div>
 
-
-    {
-        showConfirmation && (
-                <div className="confirmation-modal">
-                    <div className="modal-content">
-                        <p>Are you sure you want to delete this Wrap?</p>
-                        <button className="confirm-button" onClick={confirmDelete}>
-                            Yes
-                        </button>
-                        <button className="cancel-button" onClick={cancelDelete}>
-                            No
-                        </button>
+            {
+                showConfirmation && (
+                    <div className="confirmation-modal">
+                        <div className="modal-content">
+                            <p>Are you sure you want to delete this Wrap?</p>
+                            <button className="confirm-button" onClick={confirmDelete}>
+                                Yes
+                            </button>
+                            <button className="cancel-button" onClick={cancelDelete}>
+                                No
+                            </button>
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
         </>
     );
 };
